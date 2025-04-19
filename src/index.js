1adcfbacac5d01af03036c0d1d7ba0ef1420985c
Aadish <101470733+AadishY@users.noreply.github.com>
2025-04-19 21:32:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 const app = require('./server');
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Aadish Chat API server running on port ${PORT}`);
-});
+// Export the Express app as a serverless function
+module.exports = app;
